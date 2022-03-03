1b5e71c36c64da2115b58b43ab609540de5afcff
Methe <sabanmete57@gmail.com>
2022-03-03 23:59:31 +0300

    fallback, recevie functions, constructor, selfdestruct, hacking example, assert, require

---
+++ Contracts/FallbackFunction.sol
@@ -0,0 +1,33 @@
+
+//Copied from Smart Contract Programmer Youtube
+//we can test fallback here, if we use recevie besides fallback, if data is empty, recevie will executed
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract Fallback {
+    event Log(uint gas);
+
+    // Fallback function must be declared as external.
+    fallback() external payable {
+        // send / transfer (forwards 2300 gas to this fallback function)
+        // call (forwards all of the gas)
+        emit Log(gasleft());
+    }
+
+    // Helper function to check the balance of this contract
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
+
+contract SendToFallback {
+    function transferToFallback(address payable _to) public payable {
+        _to.transfer(msg.value);
+    }
+
+    function callFallback(address payable _to) public payable {
+        (bool sent, ) = _to.call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
+    }
+}
---
+++ Contracts/FunctionExample.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.8.0;
+//SPDX-License-Identifier: UNLICENSED
+
+//we can see the use cases of require and assert in this contract
+
+//fallback function used
+contract FunctionExample{
+
+    mapping(address=>uint) public balanceRecevied;
+
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function destroySmartContract() public{
+        require(msg.sender==owner, "You are not the owner");
+        selfdestruct(payable(owner));
+    }
+
+    function recevieMoney() public payable {
+        //balance will always be bigger than our balance + our pay, this check means we cant pay "negative" value
+        assert(balanceRecevied[msg.sender]+msg.value>=balanceRecevied[msg.sender]);
+        balanceRecevied[msg.sender] += msg.value;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public{
+        //balance needs to bigger or equal to withdraw
+        require(_amount <= balanceRecevied[msg.sender],"not enough funds");
+        //our withdraw money should be not bigger than our balance
+        assert(balanceRecevied[msg.sender]>=balanceRecevied[msg.sender] - _amount);
+        balanceRecevied[msg.sender] -= _amount;
+        _to.transfer(_amount);
+    }
+    //fallback
+    fallback() external payable{
+        recevieMoney();
+    }
+    receive() external payable {
+        recevieMoney();
+        // custom function code
+    }
+}
